225bb2545c4aa12567d1d4adb6b84c670cd69e4f
Gustavo Borges <165957766+GustavoBR09@users.noreply.github.com>
2024-10-18 18:55:46 -0300

    Iniciou API

---
+++ src/SRCM.API/Configuration/SwaggerConfig.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.OpenApi.Models;
+using System.Net;
+
+namespace SRCM.API.Configuration
+{
+    public static class SwaggerConfig
+    {
+        public static void AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new OpenApiInfo()
+                {
+                    Title = "Sistema de Registro de Consultas Médicas",
+                    Description = "Essa API foi criada na aula de programação.",
+                    Contact = new OpenApiContact()
+                    {
+                        Name = "Gustavo",
+                        Email = "gustavoborgescr09@gmail.com"
+                    }
+                });
+
+                s.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"Autenticação por cabeçalho JWT, usando o esquema Bearer
+                    Exemplo: Bearer 1234567890",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                s.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            },
+                            Scheme = "oauth2",
+                            Name = "Bearer",
+                            In = ParameterLocation.Header
+                        }, new List<string>()
+                    }
+                });
+            });
+        }
+    }
+}
